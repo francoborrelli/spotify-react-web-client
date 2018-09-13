ad678f496426fe443f526bb4107893521537529c
Franco Borrelli <francoborrelli96@outlook.com>
2018-09-13 16:06:16 -0300

    Add Footer styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,16 @@
 
 import React from 'react';
 
-const footer = props => <div className="footer"></div>
+const style =  {
+    position: "fixed",
+    width: "100%",
+    bottom: 0,
+    left: 0,
+    background: "rgb(40, 40, 40)",
+    height: 80,
+    zIndex: 2,
+  }
+
+const footer = props => <div className="footer" style={style}></div>
 
 export default footer;
