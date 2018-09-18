3d8d2e009f7e04befe9d8765ceb71a2f4efd38d0
Franco Borrelli <francoborrelli96@outlook.com>
2018-09-18 11:56:10 -0300

    Fix header responsiveness

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import UserDetails from '../userDetails/userDetails';
 import Search from '../trackSearch/trackSearch';
@@ -7,16 +7,37 @@ const style = {
   position: 'fixed',
   zIndex: 1000,
   display: 'flex',
-  width: '62%',
+  height: 50,
   justifyContent: 'space-between',
   background: 'rgb(24, 24, 24)'
 };
 
-const header = props => (
-  <div className="header" style={style}>
-    <Search />
-    <UserDetails username={props.username} img={props.img} />
-  </div>
-);
+class Header extends Component {
+  state = {
+    width: '100%'
+  };
 
-export default header;
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+    this.onResize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize = () => {
+    const width = document.getElementsByClassName('main-section-container')[0]
+      .offsetWidth;
+    this.setState({ width: width });
+  };
+
+  render = () => (
+    <div className="header" style={{ ...style, width: this.state.width }}>
+      <Search />
+      <UserDetails username={this.props.username} img={this.props.img} />
+    </div>
+  );
+}
+
+export default Header;
--- src/components/trackSearch/trackSearch.jsx
+++ src/components/trackSearch/trackSearch.jsx
@@ -1,26 +1,27 @@
-
 import React from 'react';
 
 const container = {
-	position: "relative",
-	top: "15px",
-}
+  position: 'relative',
+  top: '15px'
+};
 
 const input = {
-		background: "#fff",
-		width: 120,
-		borderRadius: 10,
-		border: "none",
-		fontFamily: "'Proxima Nova', Georgia, sans-serif",
-		padding: "4px 4px 4px 10px",
-		outline: "none",
-} 
+  background: '#fff',
+  width: 120,
+  borderRadius: 10,
+  border: 'none',
+  fontFamily: "'Proxima Nova', Georgia, sans-serif",
+  padding: '4px 4px 4px 10px',
+  outline: 'none',
+  marginTop: -2
+};
 
 const search = props => (
-<div className='track-search-container' style={container}>
-	      <form>
-	        <input type='text' placeholder='Search...' style={input}/>
-	      </form>
-</div>)
+  <div className="track-search-container" style={container}>
+    <form>
+      <input type="text" placeholder="Search..." style={input} />
+    </form>
+  </div>
+);
 
 export default search;
