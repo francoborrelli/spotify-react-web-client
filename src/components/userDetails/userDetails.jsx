c2bedbf7166655a663bd9d99afd9d8ed774f4e5d
Franco Borrelli <francoborrelli96@outlook.com>
2024-06-27 09:31:41 -0300

    add source code link

--- src/components/userDetails/userDetails.jsx
+++ src/components/userDetails/userDetails.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './userDetails.css';
 
-const header = props => (
-  <div className="details-container">
-    <img alt="user" className="user-image" src={props.img} />
-    <p className="user-name">{props.username}</p>
+export const UserHeader = (props) => (
+  <div className='details-container'>
+    <img alt='user' className='user-image' src={props.img} />
+    <p className='user-name'>{props.username}</p>
   </div>
 );
 
-export default header;
+export default UserHeader;
