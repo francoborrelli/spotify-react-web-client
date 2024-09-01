4c6b8645a8fd19c8bb133497b3b805173e24ec05
Franco Borrelli <francoborrelli96@outlook.com>
2024-09-01 13:26:45 -0300

    fetch context information

--- src/components/Layout/components/NowPlaying/index.tsx
+++ src/components/Layout/components/NowPlaying/index.tsx
@@ -5,28 +5,28 @@ import { Details } from './Details';
 // Redux
 import { memo, useEffect } from 'react';
 import { fetchQueue } from '../../../../store/slices/queue';
-import { fetchArtist } from '../../../../store/slices/playingNow';
+import { fetchDevices } from '../../../../store/slices/spotify';
 import { useAppDispatch, useAppSelector } from '../../../../store/store';
 
 export const PlayingNow = memo(() => {
   const dispatch = useAppDispatch();
   const currentSong = useAppSelector((state) => state.spotify.state?.track_window.current_track);
-  const artist = currentSong?.artists[0].uri.split(':')[2];
 
-  const devices = useAppSelector((state) => !state.spotify.devices);
   const queue = useAppSelector((state) => !state.ui.queueCollapsed);
   const details = useAppSelector((state) => !state.ui.detailsCollapsed);
+  const devices = useAppSelector((state) => !state.ui.devicesCollapsed);
 
   useEffect(() => {
-    setTimeout(() => {
+    const interval = setTimeout(() => {
       dispatch(fetchQueue());
-      if (artist) dispatch(fetchArtist(artist));
+      dispatch(fetchDevices());
     }, 1000);
-  }, [currentSong?.id, artist, dispatch]);
+    return () => clearTimeout(interval);
+  }, [currentSong?.id, dispatch]);
 
+  if (devices) return <Devices />;
   if (queue) return <Queue />;
   if (details) return <Details />;
-  if (devices) return <Devices />;
 
   return null;
 });
--- src/store/slices/playingNow.ts
+++ src/store/slices/playingNow.ts
@@ -6,13 +6,23 @@ import { artistService } from '../../services/artist';
 // Interfaces
 import type { RootState } from '../store';
 import type { Artist } from '../../interfaces/artist';
+import type { Playlist } from '../../interfaces/playlists';
+import type { AlbumFullObject } from '../../interfaces/albums';
+
+// Service
+import { albumsService } from '../../services/albums';
+import { playlistService } from '../../services/playlists';
 
 export interface PlayingNowState {
   artist: Artist | null;
+  playlist: Playlist | null;
+  album: AlbumFullObject | null;
 }
 
 const initialState: PlayingNowState = {
   artist: null,
+  album: null,
+  playlist: null,
 };
 
 export const fetchArtist = createAsyncThunk<Artist, string>(
@@ -27,6 +37,30 @@ export const fetchArtist = createAsyncThunk<Artist, string>(
   }
 );
 
+export const fetchPlaylist = createAsyncThunk<Playlist, string>(
+  'playingNow/fetchPlaylist',
+  async (id, { getState }) => {
+    const state = getState() as RootState;
+    if (state.playingNow.playlist?.id === id) {
+      return state.playingNow.playlist;
+    }
+    const response = await playlistService.getPlaylist(id);
+    return response.data;
+  }
+);
+
+export const fetchAlbum = createAsyncThunk<AlbumFullObject, string>(
+  'playingNow/fetchAlbum',
+  async (id, { getState }) => {
+    const state = getState() as RootState;
+    if (state.playingNow.album?.id === id) {
+      return state.playingNow.album;
+    }
+    const response = await albumsService.fetchAlbum(id);
+    return response.data;
+  }
+);
+
 const playingNowSlice = createSlice({
   name: 'queue',
   initialState,
@@ -35,11 +69,21 @@ const playingNowSlice = createSlice({
     builder.addCase(fetchArtist.fulfilled, (state, action) => {
       state.artist = action.payload;
     });
+    builder.addCase(fetchPlaylist.fulfilled, (state, action) => {
+      state.playlist = action.payload;
+      state.album = null;
+    });
+    builder.addCase(fetchAlbum.fulfilled, (state, action) => {
+      state.album = action.payload;
+      state.playlist = null;
+    });
   },
 });
 
 export const playingNowActions = {
+  fetchAlbum,
   fetchArtist,
+  fetchPlaylist,
   ...playingNowSlice.actions,
 };
 
