ba21f4c2be8c2cf49c71df39e36032fbbf660b39
Laurent Perroteau <l.perroteau@sbt-human.com>
2025-09-03 15:51:38 -0500

    Use firebase project (without .env.production)

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import './i18n';
-
 import TimeAgo from 'javascript-time-ago';
-
 import en from 'javascript-time-ago/locale/en';
 import es from 'javascript-time-ago/locale/es-AR';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 TimeAgo.addDefaultLocale(en);
 TimeAgo.addLocale(es);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
